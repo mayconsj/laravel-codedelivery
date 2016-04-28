9b1aacb9fe897942ca010569c14ad32a3e8790cd
Maycon Silva <mayconmsj@gmail.com>
2016-04-28 01:57:22 -0300

    Modulo Criando Base do Sistema - Clients

--- laravel-codedelivery/database/factories/ModelFactory.php
+++ laravel-codedelivery/database/factories/ModelFactory.php
@@ -26,6 +26,15 @@ $factory->define(CodeDelivery\Models\Category::class, function(Faker\Generator $
     ];
 });
 
+$factory->define(CodeDelivery\Models\Client::class, function(Faker\Generator $faker) {
+    return [
+        'phone' => $faker->phoneNumber,
+        'address' => $faker->address,
+        'city' => $faker->state,
+        'zipcode' => $faker->postcode
+    ];
+});
+
 $factory->define(CodeDelivery\Models\Product::class, function(Faker\Generator $faker) {
     return [
         'name' => $faker->word,
