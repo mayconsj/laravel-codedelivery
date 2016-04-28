d5a8f4bd1be83f791c1e0c60350754efecabae31
Maycon Silva <mayconmsj@gmail.com>
2016-04-28 12:55:24 -0300

    Modulo Sistema Administrativo - Criando nova categoria

--- laravel-codedelivery/resources/views/admin/categories/create.blade.php
+++ laravel-codedelivery/resources/views/admin/categories/create.blade.php
@@ -4,6 +4,18 @@
     <div class="container">
         <h3>Criar nova Categoria</h3>
 
+        {!! Form::open(['route'=>'admin.categories.store', 'class'=>'form']) !!}
+
+        <div class="form-group">
+            {!! Form::label('Name', 'Nome:') !!}
+            {!! Form::text('name', null,['clss'=>'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::submit('Criar Categoria', ['class'=>'btn btn-primary']) !!}
+        </div>
+
+        {!! Form::close() !!}
 
     </div>
 
