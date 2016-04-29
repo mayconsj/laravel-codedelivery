ba48d8f5734b732fb7486d1061c7e6b47a5a3c92
Maycon Silva <mayconmsj@gmail.com>
2016-04-29 14:55:04 -0300

    Modulo Checkout - Criando form de pedido - Parte 2

--- laravel-codedelivery/resources/views/customer/order/create.blade.php
+++ laravel-codedelivery/resources/views/customer/order/create.blade.php
@@ -11,7 +11,7 @@
             <div class="form-group">
                 <label>Total:</label>
                 <p id="total"></p>
-                <a href="#" class="btn btn-default">Novo Item</a>
+                <a href="#" id="btnNewItem" class="btn btn-default">Novo Item</a>
 
                 <table class="table table-bordered">
                     <thead>
@@ -25,8 +25,9 @@
                         <td>
                             <select class="form-control" name="items[0][product_id]">
                                 @foreach($products as $p)
-                                    <option value="{{$p->id}}" data-price="{{$p->price}}">{{$p->name}}
-                                        --- {{$p->price}}</option>
+                                    <option value="{{$p->id}}" data-price="{{$p->price}}">
+                                        {{$p->name}} --- {{$p->price}}
+                                    </option>
                                 @endforeach
                             </select>
                         </td>
@@ -42,4 +43,26 @@
         </div>
     </div>
 
+@endsection
+
+@section('post-script')
+    <script>
+        $('#btnNewItem').click(function () {
+            var row = $('table tbody > tr:last'),
+                    newRow = row.clone(),
+                    lenght = $("table tbody tr").length;
+
+            newRow.find('td').each(function () {
+
+                var td = $(this),
+                        input = td.find('input,select'),
+                        name = input.attr('name');
+
+                input.attr('name', name.replace((length - 1) + "", lenght + ""));
+            });
+
+            newRow.find('input').val(1);
+            newRow.insertAfter(row);
+        });
+    </script>
 @endsection
