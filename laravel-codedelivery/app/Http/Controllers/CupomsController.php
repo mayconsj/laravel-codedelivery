51960aa4cf7763b1b74b3cac332c502087948e0a
Maycon Silva <mayconmsj@gmail.com>
2016-04-29 13:43:33 -0300

    Modulo Checkout - Listando Cupoms

---
+++ laravel-codedelivery/app/Http/Controllers/CupomsController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace CodeDelivery\Http\Controllers;
+
+use CodeDelivery\Http\Requests;
+use CodeDelivery\Http\Requests\AdminCategoryRequest;
+use CodeDelivery\Models\Cupom;
+use CodeDelivery\Repositories\CupomRepository;
+
+class CupomsController extends Controller
+{
+    private $repository;
+
+    public function __construct(CupomRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function index()
+    {
+        $cupoms = $this->repository->paginate();
+        return view('admin.cupoms.index', compact('cupoms'));
+    }
+
+    public function create()
+    {
+        return view('admin.cupoms.create');
+    }
+
+    public function store(AdminCategoryRequest $request)
+    {
+        $data = $request->all();
+        $this->repository->create($data);
+        return redirect()->route('admin.categories.index');
+    }
+
+    public function edit($id)
+    {
+        $category = $this->repository->find($id);
+        return view('admin.categories.edit', compact('category'));
+    }
+
+    public function update(AdminCategoryRequest $request, $id)
+    {
+        $data = $request->all();
+        $this->repository->update($data, $id);
+        return redirect()->route('admin.categories.index');
+    }
+
+}
---
+++ laravel-codedelivery/resources/views/admin/cupoms/index.blade.php
@@ -0,0 +1,35 @@
+@extends('app')
+
+@section('content')
+    <div class="container">
+        <h3>Cupoms</h3>
+        <br><br>
+        <a href="{{route('admin.cupoms.create')}}" class="btn btn-default">Nova Cupom</a>
+        <br><br>
+        <table class="table table-bordered">
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>Código</th>
+                <th>Valor</th>
+                <th>Ação</th>
+            </tr>
+            </thead>
+
+            <tbody>
+            @foreach($cupoms as $cupom)
+                <tr>
+                    <td>{{$cupom->id}}</td>
+                    <td>{{$cupom->code}}</td>
+                    <td>{{$cupom->value}}</td>
+                    <td>--</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+
+        {!! $cupoms->render() !!}
+
+    </div>
+
+@endsection
