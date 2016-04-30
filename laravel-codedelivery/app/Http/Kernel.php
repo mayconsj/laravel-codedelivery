1b07e28ccd3917797506f1bf30d9a3d541ad89fa
Maycon Silva <mayconmsj@gmail.com>
2016-04-30 01:16:56 -0300

    Modulo Criando API de Cliente -  Testando middleware oauth.checkrole

--- laravel-codedelivery/app/Http/Kernel.php
+++ laravel-codedelivery/app/Http/Kernel.php
@@ -33,6 +33,7 @@ class Kernel extends HttpKernel
         'guest' => \CodeDelivery\Http\Middleware\RedirectIfAuthenticated::class,
 
         'auth.checkrole' => \CodeDelivery\Http\Middleware\CheckRole::class,
+        'oauth.checkrole' => \CodeDelivery\Http\Middleware\OauthCheckRole::class,
 
         'oauth' => \LucaDegasperi\OAuth2Server\Middleware\OAuthMiddleware::class,
         'oauth-user' => \LucaDegasperi\OAuth2Server\Middleware\OAuthUserOwnerMiddleware::class,
