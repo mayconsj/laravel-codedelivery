6e7be3b04b02d9a1a98b76816d272a466d677dbe
Maycon Silva <mayconmsj@gmail.com>
2016-05-05 19:59:51 -0300

    Modulo Area de Pedidos -  Criando  - parte 3

--- laravel-codedelivery/ionic/www/js/controllers/services/cart.js
+++ laravel-codedelivery/ionic/www/js/controllers/services/cart.js
@@ -17,11 +17,32 @@ angular.module('starter.services')
         };
 
         this.addItem = function (item) {
+            var cart = this.get(), itemAux, exists = false;
+            for (var index in cart.items) {
+                itemAux = cart.items[index];
+                if (itemAux.id == item.id) {
+                    itemAux.qtd = item.qtd + itemAux.qtd;
+                    itemAux.subTotal = calculateSubTotal(itemAux);
+                    exists = true;
+                    break;
+                }
+                ;
+            }
 
+            if (!exists) {
+                item.subtotal = calculateSubTotal(item);
+                cart.items.push(item);
+            }
+
+            $localStorage.setObject(key, cart);
         };
 
         this.removeItem = function (i) {
 
         };
+
+        function calculateSubTotal(item) {
+            return item.price * item.qtd;
+        }
 
     }]);
