495ab9b683f2609f5af9b299d92b1a214519894d
Maycon Silva <mayconmsj@gmail.com>
2016-05-05 19:46:35 -0300

    Modulo Area de Pedidos -  Criando  - parte 2

--- laravel-codedelivery/ionic/www/js/controllers/services/cart.js
+++ laravel-codedelivery/ionic/www/js/controllers/services/cart.js
@@ -1,15 +1,19 @@
 angular.module('starter.services')
     .service('$cart', ['$localStorage', function ($localStorage) {
+        var key = 'kart';
         this.clear = function () {
-
+            $localStorage.setObject(key, {
+                items: [],
+                total: 0
+            })
         };
 
         this.get = function () {
-
+            return $localStorage.getObject(key);
         };
 
         this.getItem = function (i) {
-
+            return this.get().items[i];
         };
 
         this.addItem = function (item) {
