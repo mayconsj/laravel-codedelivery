9decf95b8e55efe6fcb4f8d056d4d1b666ba2928
Maycon Silva <mayconmsj@gmail.com>
2016-05-05 17:01:01 -0300

    Modulo Area de Pedidos - Configurando Views e Controllers

--- laravel-codedelivery/ionic/www/js/app.js
+++ laravel-codedelivery/ionic/www/js/app.js
@@ -3,6 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
+angular.module('starter.controllers', []);
 angular.module('starter', ['ionic', 'starter.controllers', 'angular-oauth2'])
 
     .run(function ($ionicPlatform) {
@@ -73,7 +74,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'angular-oauth2'])
             .state('client.view_products', {
                 url: '/view_products',
                 templateUrl: 'templates/client/view_products.html',
-                controller: 'ClientViewProductsCtrl'
+                controller: 'ClientViewProductCtrl'
             })
         //$urlRouterProvider.otherwise('/');
     });
