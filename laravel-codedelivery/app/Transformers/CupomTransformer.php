e97ce05d588bc0a08b7fe340c7523e79501294fa
Maycon Silva <mayconmsj@gmail.com>
2016-05-08 01:18:42 -0300

    Modulo Cupom QRCODE - Consultando cupons na API - parte 1

--- laravel-codedelivery/app/Transformers/CupomTransformer.php
+++ laravel-codedelivery/app/Transformers/CupomTransformer.php
@@ -21,8 +21,9 @@ class CupomTransformer extends TransformerAbstract
     public function transform(Cupom $model)
     {
         return [
-            'id'         => (int) $model->id,
-
+            'id'    => (int)$model->id,
+            'code'  => $model->code,
+            'value' => (float) $model->value,
             /* place your other model properties here */
 
             'created_at' => $model->created_at,
